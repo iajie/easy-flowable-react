35b6862440d1bf726d4129e5316bd173f66046a5
阿杰 <ajie20999@163.com>
2024-10-26 11:11:03 +0800

    fix: 将属性面板放到设计器整体，而非设计器位置包涵了工具栏的位置

--- src/Bpmn/index.tsx
+++ src/Bpmn/index.tsx
@@ -98,9 +98,10 @@ export default ({ height = 60, align = 'default', bpmnStyle = {}, ...props }: Bp
     }, []);
 
     return <div style={{ width: "100%", position: 'relative' }}>
-        {(modeler && props.toolbarRender !== false) && (props.toolbarRender || <Toolbar {...props.toolbar} modeler={modeler} bpmnData={bpmnData} uploadXml={(xml) => setXml(xml)} />)}
+        {(modeler && props.toolbarRender !== false) && ((props.panelRender && props.panelRender(modeler)) ||
+            <Toolbar {...props.toolbar} modeler={modeler} bpmnData={bpmnData} uploadXml={(xml) => setXml(xml)} />)}
         <div id="container" ref={containerRef} style={{ ...bpmnDefaultStyle, ...bpmnStyle }}/>
-        {(modeler && defaultElement && props.panelRender !== false) &&
-            ((props.panelRender && props.panelRender(modeler)) || <PropertiesPanel { ...props.panel } modeler={modeler} defaultElement={defaultElement} bpmnInfo={(data) => setBpmnData(data)} />)}
+        {(modeler && defaultElement && props.panelRender !== false) && ((props.panelRender && props.panelRender(modeler)) ||
+                <PropertiesPanel { ...props.panel } modeler={modeler} defaultElement={defaultElement} bpmnInfo={(data) => setBpmnData(data)} />)}
     </div>
 }
